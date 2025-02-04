ee67ce21d077779c91da1cf84ec16843f7a7b196
Dailson Campos <dcampos.dev@gmail.com>
2025-02-04 19:21:27 -0300

    Add cart routes for managing products: add, update, and remove items

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
+
   resources :products
+
+  resource :cart, only: [:show] do
+    post 'add_item', to: 'carts#add_product'
+    patch 'update_item', to: 'carts#update_item'
+    delete 'remove_product/:product_id', to: 'carts#remove_product'
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "rails/health#show"

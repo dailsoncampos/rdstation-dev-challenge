e6ed935aab362ba13faf8f6ec5e54c16383e4e1c
Dailson Campos <dcampos.dev@gmail.com>
2025-02-04 19:20:38 -0300

    Add validations and total_price method to CartItem model; create factory and specs

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe CartItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:cart) { create(:cart) }
+  let(:product) { create(:product, price: 10.0) }
+  let(:cart_item) { create(:cart_item, cart: cart, product: product, quantity: 2) }
+
+  describe "associations" do
+    it { should belong_to(:cart) }
+    it { should belong_to(:product) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:quantity) }
+    it { should validate_numericality_of(:quantity).only_integer.is_greater_than(0) }
+  end
+
+  describe "#total_price" do
+    it "calculates the total price correctly" do
+      expect(cart_item.total_price).to eq(20.0)
+    end
+  end
 end

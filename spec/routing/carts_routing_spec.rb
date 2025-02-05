d037106d21606588984cf8a3ef0284b51d30fa1c
Dailson Campos <dcampos.dev@gmail.com>
2025-02-05 00:01:56 -0300

    Update routing specs for cart actions: add product, update item, and remove product

--- spec/routing/carts_routing_spec.rb
+++ spec/routing/carts_routing_spec.rb
@@ -6,12 +6,16 @@ RSpec.describe CartsController, type: :routing do
       expect(get: '/cart').to route_to('carts#show')
     end
 
-    it 'routes to #create' do
-      pending "#TODO: Escreva um teste para validar a criação de um carrinho #{__FILE__}" 
+    it 'routes to #add_product via POST' do
+      expect(post: '/cart/add_item').to route_to('carts#add_product')
     end
 
-    it 'routes to #add_item via POST' do
-      expect(post: '/cart/add_item').to route_to('carts#add_item')
+    it 'routes to #update_item via PATCH' do
+      expect(patch: '/cart/update_item').to route_to('carts#update_item')
+    end
+
+    it 'routes to #remove_product via DELETE' do
+      expect(delete: '/cart/remove_product/1').to route_to('carts#remove_product', product_id: '1')
     end
   end
-end 
+end

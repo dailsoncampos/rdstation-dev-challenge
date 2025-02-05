02007dd7e2a71822e0633c20af66dcd23ab9b5d6
Dailson Campos <dcampos.dev@gmail.com>
2025-02-04 23:50:14 -0300

    Implement abandoned cart functionality with methods to mark and remove abandoned carts

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,8 +1,25 @@
 class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
-  validates_numericality_of :total_price, greater_than_or_equal_to: 0
+
+  validates :total_price, numericality: { greater_than_or_equal_to: 0 }
+
+  before_save :update_total_price
 
   def total_price
     cart_items.sum(&:total_price)
+  end
+
+  def mark_as_abandoned
+    update(abandoned: true) if last_interaction_at < 3.hours.ago
+  end
+
+  def remove_if_abandoned
+    destroy if abandoned? && last_interaction_at < 7.days.ago
+  end
+
+  private
+
+  def update_total_price
+    self.total_price = total_price
   end
 end

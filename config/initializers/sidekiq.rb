e5fb8e24f34e87e1c280e424222b0c5ac40eb6be
Dailson Campos <dcampos.dev@gmail.com>
2025-02-05 23:34:08 -0300

    Add Sidekiq configuration and scheduler for marking abandoned carts

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,14 @@
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0') }
+  config.on(:startup) do
+    schedule_file = "config/sidekiq.yml"
+
+    if File.exist?(schedule_file)
+      Sidekiq::Scheduler.reload_schedule!
+    end
+  end
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0') }
+end

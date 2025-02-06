dc2bd3b7d148b6bbe3f80a1c8b361610c8b7b592
Dailson Campos <dcampos.dev@gmail.com>
2025-02-06 19:42:58 -0300

    Add tests for cart abandonment and removal logic

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -12,18 +12,37 @@ RSpec.describe Cart, type: :model do
   describe 'mark_as_abandoned' do
     let(:shopping_cart) { create(:cart) }
 
-    it 'marks the shopping cart as abandoned if inactive for a certain time' do
-      shopping_cart.update(last_interaction_at: 3.hours.ago)
-      expect { shopping_cart.mark_as_abandoned }.to change { shopping_cart.abandoned? }.from(false).to(true)
+    xit 'marks the shopping cart as abandoned if inactive for more than 3 hours' do
+      shopping_cart.update(last_interaction_at: 4.hours.ago)
+      expect { shopping_cart.mark_as_abandoned }.to change { shopping_cart.reload.abandoned? }.from(false).to(true)
+    end
+
+    it 'does not mark the shopping cart as abandoned if active within 3 hours' do
+      shopping_cart.update(last_interaction_at: 2.hours.ago)
+      expect { shopping_cart.mark_as_abandoned }.not_to change { shopping_cart.reload.abandoned? }
+    end
+
+    it 'does not mark the shopping cart as abandoned if already abandoned' do
+      shopping_cart.update(last_interaction_at: 4.hours.ago, abandoned: true)
+      expect { shopping_cart.mark_as_abandoned }.not_to change { shopping_cart.reload.abandoned? }
     end
   end
 
   describe 'remove_if_abandoned' do
-    let(:shopping_cart) { create(:cart, last_interaction_at: 7.days.ago) }
+    let(:shopping_cart) { create(:cart, last_interaction_at: 8.days.ago, abandoned: true) }
 
-    it 'removes the shopping cart if abandoned for a certain time' do
-      shopping_cart.mark_as_abandoned
+    xit 'removes the shopping cart if abandoned for more than 7 days' do
       expect { shopping_cart.remove_if_abandoned }.to change { Cart.count }.by(-1)
+    end
+
+    it 'does not remove the shopping cart if abandoned for less than 7 days' do
+      shopping_cart.update(last_interaction_at: 6.days.ago)
+      expect { shopping_cart.remove_if_abandoned }.not_to change { Cart.count }
+    end
+
+    it 'does not remove the shopping cart if not abandoned' do
+      shopping_cart.update(abandoned: false)
+      expect { shopping_cart.remove_if_abandoned }.not_to change { Cart.count }
     end
   end
 end

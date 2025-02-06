cb0d25982edd382b271a99675b838748fd84926c
Dailson Campos <dcampos.dev@gmail.com>
2025-02-06 19:43:49 -0300

    Add test for MarkCartAsAbandonedJob to verify CartAbandonmentService call

--- spec/sidekiq/mark_cart_as_abandoned_job_spec.rb
+++ spec/sidekiq/mark_cart_as_abandoned_job_spec.rb
@@ -1,3 +1,8 @@
 require 'rails_helper'
+
 RSpec.describe MarkCartAsAbandonedJob, type: :job do
+  it "calls the CartAbandonmentService to mark and remove abandoned carts" do
+    expect(CartAbandonmentService).to receive(:call)
+    described_class.new.perform
+  end
 end

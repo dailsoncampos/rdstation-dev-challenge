282b9350ccb222d0cff183b508fbc0caaca17e31
Dailson Campos <dcampos.dev@gmail.com>
2025-02-09 17:47:07 -0300

    Refactor CartItem model specs to improve validation tests and enhance total_price calculation

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -1,23 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe CartItem, type: :model do
-  let(:cart) { create(:cart) }
-  let(:product) { create(:product, price: 10.0) }
-  let(:cart_item) { create(:cart_item, cart: cart, product: product, quantity: 2) }
-
-  describe "associations" do
-    it { should belong_to(:cart) }
-    it { should belong_to(:product) }
-  end
+  let(:cart_item) { create(:cart_item) }
 
   describe "validations" do
-    it { should validate_presence_of(:quantity) }
-    it { should validate_numericality_of(:quantity).only_integer.is_greater_than(0) }
+    it "is valid with a positive quantity" do
+      expect(cart_item).to be_valid
+    end
+
+    it "is invalid with a negative or zero quantity" do
+      cart_item.quantity = 0
+      expect(cart_item).to_not be_valid
+
+      cart_item.quantity = -2
+      expect(cart_item).to_not be_valid
+    end
   end
 
   describe "#total_price" do
-    it "calculates the total price correctly" do
-      expect(cart_item.total_price).to eq(20.0)
+    it "correctly calculates the total price of the item" do
+      product = create(:product, price: 15.0)
+      cart_item = create(:cart_item, product: product, quantity: 3)
+      expect(cart_item.total_price).to eq(45.0)
     end
   end
 end

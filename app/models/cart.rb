9a403fd9a1a3f0070848fe2354a15afa067591cb
Dailson Campos <dcampos.dev@gmail.com>
2025-02-09 17:44:04 -0300

    Refactor Cart model to improve validation callbacks and enhance add_product method

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,26 +3,31 @@ class Cart < ApplicationRecord
 
   validates :total_price, numericality: { greater_than_or_equal_to: 0 }
 
-  before_save :update_total_price
-  before_save :update_last_interaction_at
+  before_validation :update_last_interaction_at, on: [:create, :update]
 
   def total_price
     cart_items.sum(&:total_price)
   end
 
   def mark_as_abandoned
-    update(abandoned: true) if !abandoned? && last_interaction_at && last_interaction_at < 3.hours.ago
+    update!(abandoned: true) if !abandoned? && last_interaction_at && last_interaction_at < 3.hours.ago
   end
 
   def remove_if_abandoned
-    destroy if abandoned? && last_interaction_at && last_interaction_at < 7.days.ago
+    destroy! if abandoned? && last_interaction_at && last_interaction_at < 7.days.ago
+  end
+
+  def add_product(product, quantity)
+    return nil if quantity.nil? || quantity <= 0
+  
+    cart_item = cart_items.find_or_initialize_by(product: product)
+    cart_item.quantity = (cart_item.quantity.presence || 0) + quantity
+    cart_item.save!
+  
+    cart_item
   end
 
   private
-
-  def update_total_price
-    self.total_price = total_price
-  end
 
   def update_last_interaction_at
     self.last_interaction_at = Time.current
